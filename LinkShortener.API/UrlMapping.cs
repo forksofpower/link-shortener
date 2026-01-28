742120ab08a37cd8f1edd0c60fa122b0362f31e4
Patrick Jones <forksofpower@gmail.com>
2026-01-28 01:19:25 -0600

    set max lengths for strings on UrlMapping

--- LinkShortener.API/UrlMapping.cs
+++ LinkShortener.API/UrlMapping.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LinkShortener.API;
 
 public class UrlMapping
 {
     public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(120)]
     public string LongUrl { get; set; } = string.Empty;
-    public string ShortUrl { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(7)]
+    public string ShortCode { get; set; } = string.Empty;
 }
