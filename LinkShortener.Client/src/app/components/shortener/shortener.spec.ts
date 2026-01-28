b0ac709fe9cd9c63dd5af8ee105887837132d0a5
Patrick Jones <forksofpower@gmail.com>
2026-01-28 02:24:23 -0600

    add Shortener component test

---
+++ LinkShortener.Client/src/app/components/shortener/shortener.spec.ts
@@ -0,0 +1,53 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Shortener } from './shortener';
+import { describe, it, expect, beforeEach, type Mock, vi } from 'vitest';
+import {FormsModule} from '@angular/forms';
+import {UrlService} from '../../services/url.service';
+import {of} from 'rxjs';
+import {By} from '@angular/platform-browser';
+
+
+describe('Shortener Component', () => {
+  let component: Shortener;
+  let fixture: ComponentFixture<Shortener>;
+  let mockUrlService: { shortenUrl: Mock };
+
+  beforeEach(async () => {
+    mockUrlService = {
+      shortenUrl: vi.fn()
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [Shortener, FormsModule],
+      providers: [
+        { provide: UrlService, useValue: mockUrlService },
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Shortener);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should call service when button is clicked', () => {
+    // create mock return value
+    mockUrlService.shortenUrl.mockReturnValue(of({ shortCode: 'abc1234' }));
+
+    // type into the input
+    const inputEl = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputEl.value = 'https://wikipedia.org';
+    inputEl.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    // click the button
+    const buttonEl = fixture.debugElement.query(By.css('button')).nativeElement;
+    buttonEl.click();
+    fixture.detectChanges();
+
+    // assertions
+    expect(mockUrlService.shortenUrl).toHaveBeenCalled();
+
+    const linkEl = fixture.debugElement.query(By.css('a')).nativeElement;
+    expect(linkEl.textContent).toContain('abc1234');
+  });
+});
